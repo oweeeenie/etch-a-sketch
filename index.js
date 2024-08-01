53c8170f98516a0ccf8c0d10e69493c671368ae0
oweeeenie <owensullivan419@yahoo.com>
2024-07-31 20:25:28 -0400

    made the website change background color! also aligned it with the header and added a reset button

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const container = document.querySelector("#square-container");
+const header = document.createElement("div");
+header.id = "header";
+document.body.prepend(header);
 
 for (let div_square = 0; div_square < 256; div_square++) {
     let gridSquare = document.createElement("div");
     container.appendChild(gridSquare);
 }
+
+const gridDivs = document.querySelectorAll("#square-container div");
+gridDivs.forEach(div => {
+    div.addEventListener("mouseover" , () => {
+        div.style.backgroundColor = "wheat";
+    });
+});
+
+const resetButton = document.createElement("button");
+resetButton.textContent = "Reset Grid";
+header.appendChild(resetButton);
